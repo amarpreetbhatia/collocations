e1404870c03f8855cd4609edec6422cdd81f3b20
amarpreetbhatia <amarpreetbhatia@gmail.com>
2017-07-08 00:05:23 +0530

    word with synonyms added

--- src/main/java/com/amarpreet/AppDataLoader.java
+++ src/main/java/com/amarpreet/AppDataLoader.java
@@ -2,8 +2,11 @@ package com.amarpreet;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.StringJoiner;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -16,16 +19,16 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.amarpreet.model.CollocationModel;
 import com.amarpreet.model.WordList;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @Component
@@ -60,27 +63,55 @@ public class AppDataLoader implements ApplicationRunner{
 		populateSynonyms();
 	}
 	
-	private void populateSynonyms(){
+	private void populateSynonyms() throws IOException{
 		RestTemplate restTemplate = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Accept",MediaType.APPLICATION_JSON_VALUE);
 		headers.set("app_id", "e5da5288");
 		headers.set("app_key", "1c9c9a30ff1b70082a9e5cd2ddff6af4");
 		HttpEntity<String> requestEntity = new HttpEntity<String>(headers);
-		//ObjectMapper objectMapper = new ObjectMapper();
-		//try {
-			//JsonNode readValue = objectMapper.readValue(new File(getClass().getClassLoader().getResource("static/sampleresponse.json").getFile()), JsonNode.class);
-	/*	} catch (IOException e) {
-			// TODO Auto-generated catch block
+		FileWriter fileWriter = new FileWriter("C:\\Users\\YCN\\workspace-sts\\collocations\\src\\main\\resources\\static\\WordList.txt");
+		FileWriter fileNotFoundWriter = new FileWriter("C:\\Users\\YCN\\workspace-sts\\collocations\\src\\main\\resources\\static\\notfoundWordList.txt");
+		
+		Enumeration<String> keys = concurrentHashMap.keys();
+		while(keys.hasMoreElements()){
+		
+		ResponseEntity<JsonNode> result = null;
+		String w = keys.nextElement();
+		
+		try{
+		result = restTemplate.exchange("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + w + "/synonyms;antonyms",HttpMethod.GET, requestEntity,JsonNode.class);
+		}catch(HttpClientErrorException e){
+			result = ResponseEntity.notFound().build();
+			fileNotFoundWriter.write(w);
+			fileNotFoundWriter.flush();
+			System.out.println("e for " + w);	
+		}
+		if(result.getStatusCode().equals(HttpStatus.OK)){
+			JsonNode readValue = result.getBody();
+		//	System.out.println("--->" + readValue.get("results").get(0).get("id"));
+		//	System.out.println("--->" + readValue.get("results").get(0).get("lexicalEntries").get(0).get("entries").get(0).get("senses").get(0).get("synonyms").toString());
+			String word = readValue.get("results").get(0).get("id").toString();
+			StringJoiner synlist = new StringJoiner(",");
+			Iterator<JsonNode> synonymsList = readValue.get("results").get(0).get("lexicalEntries").get(0).get("entries").get(0).get("senses").get(0).get("synonyms").elements();
+			
+			while(synonymsList.hasNext()){
+			//	System.out.println("I am inside");
+				synlist.add(synonymsList.next().get("id").toString());
+			}
+		
+			fileWriter.write(word + "," +synlist.toString() + System.getProperty("line.separator"));
+			fileWriter.flush();
+			
+		}
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	*/	
-	//	restTemplate.getMessageConverters().add(0,new MappingJackson2HttpMessageConverter(objectMapper));
-		ResponseEntity<JsonNode> result = restTemplate.exchange("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace/synonyms;antonyms",HttpMethod.GET, requestEntity,JsonNode.class);
-		JsonNode readValue = result.getBody();
-		System.out.println("--->" + readValue.get("results").get(0).get("id"));
-		System.out.println("--->" + readValue.get("results").get(0).get("lexicalEntries").get(0).get("entries").get(0).get("senses").get(0).get("synonyms").toString());
-	
+		}
+		fileWriter.close();
+		fileNotFoundWriter.close();
 	}
 	
 	
